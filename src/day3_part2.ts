101301d3bd5df7034f101d35fb99b8cc861d7ea4
thanos <chaddwick25@yahoo.ca>
2022-09-28 19:09:03 -0400

    Completed Day 3: Binary Diagnostic (Part 2)

--- src/day3_part2.ts
+++ src/day3_part2.ts
@@ -20,7 +20,6 @@ Each bit in the gamma rate can be determined by finding the most common bit in t
 11001
 00010
 01010
-
 Considering only the first bit of each number, there are five 0 bits and seven 1 bits. Since the most common bit is 1, the first bit of the gamma rate is 1.
 
 The most common second bit of the numbers in the diagnostic report is 0, so the second bit of the gamma rate is 0.
@@ -33,10 +32,33 @@ The epsilon rate is calculated in a similar way; rather than use the most common
 
 Use the binary numbers in your diagnostic report to calculate the gamma rate and epsilon rate, then multiply them together. What is the power consumption of the submarine? (Be sure to represent your answer in decimal, not binary.)
 
+Your puzzle answer was 3895776.
+
+The first half of this puzzle is complete! It provides one gold star: *
+
+--- Part Two ---
+Next, you should verify the life support rating, which can be determined by multiplying the oxygen generator rating by the CO2 scrubber rating.
+
+Both the oxygen generator rating and the CO2 scrubber rating are values that can be found in your diagnostic report - finding them is the tricky part. Both values are located using a similar process that involves filtering out values until only one remains. Before searching for either rating value, start with the full list of binary numbers from your diagnostic report and consider just the first bit of those numbers. Then:
+
+Keep only numbers selected by the bit criteria for the type of rating value for which you are searching. Discard numbers which do not match the bit criteria.
+If you only have one number left, stop; this is the rating value for which you are searching.
+Otherwise, repeat the process, considering the next bit to the right.
+The bit criteria depends on which type of rating value you want to find:
+
+To find oxygen generator rating, determine the most common value (0 or 1) in the current bit position, and keep only numbers with that bit in that position. If 0 and 1 are equally common, keep values with a 1 in the position being considered.
+To find CO2 scrubber rating, determine the least common value (0 or 1) in the current bit position, and keep only numbers with that bit in that position. If 0 and 1 are equally common, keep values with a 0 in the position being considered.
+For example, to determine the oxygen generator rating value using the same example diagnostic report from above:
+
+Start with all 12 numbers and consider only the first bit of each number. There are more 1 bits (7) than 0 bits (5), so keep only the 7 numbers with a 1 in the first position: 11110, 10110, 10111, 10101, 11100, 10000, and 11001.
+Then, consider the second bit of the 7 remaining numbers: there are more 0 bits (4) than 1 bits (3), so keep only the 4 numbers with a 0 in the second position: 10110, 10111, 10101, and 10000.
+In the third position, three of the four numbers have a 1, so keep those three: 10110, 10111, and 10101.
+In the fourth position, two of the three numbers have a 1, so keep those two: 10110 and 10111.
+In the fifth position, there are an equal number of 0 bits and 1 bits (one each). So, to find the oxygen generator rating, keep the number with a 1 in that position: 10111.
+As there is only one number left, stop; the oxygen generator rating is 10111, or 23 in decimal.
  */
 
 import { binaryPositions } from './constants/binary_positions';
-
 
 let result: string = '';
 
@@ -48,7 +70,9 @@ const reducePositons = (index:number,  positions: string[]) => {
     let total;
     for (let i = 0; i < positions.length; i++){
         const position = positions[i];
-        if(Number(position[index]) === 0){    
+        // console.log(Number(position[index]));
+        
+        if(Number(position[index]) === 1 ){    
             ones.push(position)
             count +=1;
         }else{
@@ -58,7 +82,8 @@ const reducePositons = (index:number,  positions: string[]) => {
         // at the end of the loop
         if(i === (positions.length - 1)){
             // determine whether to use ones or zereos
-            total = count > (positions.length / 2) ? ones : zeroes
+            total = count >= (positions.length / 2) ? ones : zeroes
+            console.log(total);
             if(total.length == 1 && index === (position.length - 1 )){
                 result = total[0]
             }else{
